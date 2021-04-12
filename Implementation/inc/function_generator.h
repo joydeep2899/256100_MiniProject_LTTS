f21482ecf6a37a60865e0c159796f19fe7a0fe80
MeghanaMeda <36812612+MeghanaMeda@users.noreply.github.com>
2021-04-12 22:23:10 +0530

    Add files via upload

--- Implementation/inc/function_generator.h
+++ Implementation/inc/function_generator.h
@@ -1,1 +1,73 @@
 
+#ifndef __FUNCTION_GENERATOR_H__
+#define __FUNCTION_GENERATOR_H__ 
+
+#include <stdio.h>
+#include <math.h>
+/**
+ * @brief 
+ * 
+ * @param time 
+ * @param frequency 
+ * @param amplitude 
+ * @return double 
+ */
+double generate_sine_wave(double time, double frequency, double amplitude);
+/**
+ * @brief 
+ * 
+ * @param time 
+ * @param frequency 
+ * @param amplitude 
+ * @return double 
+ */
+double generate_amplitude_sine(double time, double frequency, double amplitude);
+/**
+ * @brief 
+ * 
+ * @param time 
+ * @param slope 
+ * @param frequency 
+ * @param amplitude 
+ * @return double 
+ */
+double generate_sawtooth_wave(double time, double slope, double frequency, double amplitude);
+/**
+ * @brief 
+ * 
+ * @param time 
+ * @param slope 
+ * @param frequency 
+ * @param amplitude 
+ * @return double 
+ */
+double generate_amplitude_sawtooth(double time, double slope, double frequency, double amplitude);
+/**
+ * @brief 
+ * 
+ * @param time 
+ * @param frequency 
+ * @param amplitude 
+ * @return double 
+ */
+double generate_square_wave(double time, double frequency, double amplitude);
+/**
+ * @brief 
+ * 
+ * @param time 
+ * @param frequency 
+ * @param amplitude 
+ * @return double 
+ */
+double generate_amplitude_square(double time, double frequency, double amplitude);
+/**
+ * @brief 
+ * 
+ * @param time 
+ * @param frequency 
+ * @param amplitude 
+ * @return double 
+ */
+double generate_triangular_wave(double time, double frequency, double amplitude);
+
+#endif
