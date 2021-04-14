b7f5b33262cb96ba5809d1ea19e07d35b615f9ff
MeghanaMeda <36812612+MeghanaMeda@users.noreply.github.com>
2021-04-14 18:23:59 +0530

    Add files via upload

--- Implementation/inc/function_generator.h
+++ Implementation/inc/function_generator.h
@@ -4,70 +4,21 @@
 
 #include <stdio.h>
 #include <math.h>
-/**
- * @brief 
- * 
- * @param time 
- * @param frequency 
- * @param amplitude 
- * @return double 
- */
-double generate_sine_wave(double time, double frequency, double amplitude);
-/**
- * @brief 
- * 
- * @param time 
- * @param frequency 
- * @param amplitude 
- * @return double 
- */
+
+int generate_sine_wave(double frequency, double amplitude);
+
 double generate_amplitude_sine(double time, double frequency, double amplitude);
-/**
- * @brief 
- * 
- * @param time 
- * @param slope 
- * @param frequency 
- * @param amplitude 
- * @return double 
- */
-double generate_sawtooth_wave(double time, double slope, double frequency, double amplitude);
-/**
- * @brief 
- * 
- * @param time 
- * @param slope 
- * @param frequency 
- * @param amplitude 
- * @return double 
- */
+
+int generate_sawtooth_wave(double frequency, double amplitude);
+
 double generate_amplitude_sawtooth(double time, double slope, double frequency, double amplitude);
-/**
- * @brief 
- * 
- * @param time 
- * @param frequency 
- * @param amplitude 
- * @return double 
- */
-double generate_square_wave(double time, double frequency, double amplitude);
-/**
- * @brief 
- * 
- * @param time 
- * @param frequency 
- * @param amplitude 
- * @return double 
- */
+
+int generate_square_wave(double frequency, double amplitude);
+
 double generate_amplitude_square(double time, double frequency, double amplitude);
-/**
- * @brief 
- * 
- * @param time 
- * @param frequency 
- * @param amplitude 
- * @return double 
- */
-double generate_triangular_wave(double time, double frequency, double amplitude);
+
+int generate_triangular_wave( double frequency, double amplitude);
+
+double generate_amplitude_triangle(double time, double slope1, double slope2, double frequency, double amplitude);
 
 #endif
